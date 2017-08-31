7aaaac6f07be884c5309d42351060e94f4241e85
Gazi2014 <abmgmohiuddin@gmail.com>
2017-08-31 13:13:09 +0600

    sfdsf

---
+++ Test.sql
@@ -0,0 +1,27 @@
+﻿/*
+   Thursday, August 31, 20171:11:49 PM
+   User: 
+   Server: GAZI2017\SQLEXPRESS
+   Database: EnvironWeb
+   Application: 
+*/
+
+/* To prevent any potential data loss issues, you should review this script in detail before running it outside the context of the database designer.*/
+BEGIN TRANSACTION
+SET QUOTED_IDENTIFIER ON
+SET ARITHABORT ON
+SET NUMERIC_ROUNDABORT OFF
+SET CONCAT_NULL_YIELDS_NULL ON
+SET ANSI_NULLS ON
+SET ANSI_PADDING ON
+SET ANSI_WARNINGS ON
+COMMIT
+BEGIN TRANSACTION
+GO
+ALTER TABLE dbo.Gazi ADD
+	Test varchar(50) NULL,
+	TestTwo varchar(50) NULL
+GO
+ALTER TABLE dbo.Gazi SET (LOCK_ESCALATION = TABLE)
+GO
+COMMIT
